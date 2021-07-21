8254e086e6940d685084b360dd7b0d4e921c1256
bwk-patterntech <bonwook.koo@patterntech.co.kr>
2021-07-22 04:51:46 +0900

    End

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var cookie = require('cookie');
+
+function authIsOwner(request, response){
+    var isOwner = false;
+    var cookies = {}
+    if(request.headers.cookie){
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
+    if(cookies.email === 'bonwook.koo@patterntech.co.kr' && cookies.password === '0816'){
+        isOwner = true;
+    }
+    console.log(isOwner);
+
+    return isOwner;
+}
+
+function authStatusUI(request, response){
+    var authStatusUI = '<a href="/login">login</a>';
+    if(authIsOwner(request, response)){
+        authStatusUI = '<a href="/logout_process">logout</a>'
+    }
+    return authStatusUI;
+}
 
 var app = http.createServer(function(request, response) {
     var _url = request.url;
@@ -16,7 +40,7 @@ var app = http.createServer(function(request, response) {
                 var title = 'Welcome';
                 var description = "Hello, Node.js";
                 var list = template.list(filelist);
-                var html = template.html(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+                var html = template.html(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`, authStatusUI(request, response));
                 response.writeHead(200);
                 response.end(html);
             });   
@@ -33,13 +57,17 @@ var app = http.createServer(function(request, response) {
                     <form action="delete_process" method="post">
                         <input type="hidden" name="id" value="${sanitizedTitle}">
                         <input type="submit" value="delete">
-                    </form>`);
+                    </form>`, authStatusUI(request, response));
                     response.writeHead(200);
                     response.end(html);
                 });
             });                
         }
     } else if(pathname === '/create') {
+        if(authIsOwner(request, response) === false){
+            response.end('Login required!!');
+            return false;
+        }
         fs.readdir('./data', function(error, filelist){
             var title = 'WEB - create';
             var list = template.list(filelist);
@@ -51,11 +79,15 @@ var app = http.createServer(function(request, response) {
                 <p>
                     <input type="submit">
                 </p>
-            </form>`, '');
+            </form>`, '', authStatusUI(request, response));
             response.writeHead(200);
             response.end(html);
         });
     } else if(pathname === '/create_process') {
+        if(authIsOwner(request, response) === false){
+            response.end('Login required!!');
+            return false;
+        }
         var body = '';
         request.on('data', function(data){
             body = body + data;
@@ -71,6 +103,10 @@ var app = http.createServer(function(request, response) {
             })
         });
     } else if(pathname === '/update') {
+        if(authIsOwner(request, response) === false){
+            response.end('Login required!!');
+            return false;
+        }
         fs.readdir('./data', function(error, filelist){
             var filteredId = path.parse(queryData.id).base
             fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
@@ -90,12 +126,16 @@ var app = http.createServer(function(request, response) {
                     </p>
                  </form>
                 `, 
-                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`, authStatusUI(request, response));
                 response.writeHead(200);
                 response.end(html);
             });
         });
     } else if(pathname === '/update_process') {
+        if(authIsOwner(request, response) === false){
+            response.end('Login required!!');
+            return false;
+        }
         var body = '';
         request.on('data', function(data){
             body = body + data;
@@ -115,6 +155,10 @@ var app = http.createServer(function(request, response) {
            console.log(post);
         });
     } else if(pathname === '/delete_process') {
+        if(authIsOwner(request, response) === false){
+            response.end('Login required!!');
+            return false;
+        }
         var body = '';
         request.on('data', function(data){
             body = body + data;
@@ -127,6 +171,55 @@ var app = http.createServer(function(request, response) {
                 response.writeHead(302, {Location: `/`}); // redirect
                 response.end();
             })
+        });
+    } else if(pathname === '/login'){
+        fs.readdir('./data', function(error, filelist){
+            var title = 'Login';
+            var list = template.list(filelist);
+            var html = template.html(title, list, `<form action="/login_process" method="post">
+                <p><input type="text" name="email" placeholder="email"</p>
+                <p><input type="password" name="password" placeholder="password"</p>
+                <p><input type="submit"</p>`, ``);
+            response.writeHead(200);
+            response.end(html);
+        });
+    } else if(pathname === '/login_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            if(post.email === 'bonwook.koo@patterntech.co.kr' && post.password === '0816') {
+                response.writeHead(302, {
+                    'Set-Cookie': [
+                        `email=${post.email}`,
+                        `password=${post.password}`,
+                        'nickname=bonwook'
+                    ],
+                    Location: '/'
+                });
+                response.end();
+            } else {
+                response.end('Who?');
+            }
+        });
+    } else if(pathname === '/logout_process') {
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            response.writeHead(302, {
+                'Set-Cookie': [
+                    `email=; Max-Age=0`,
+                    `password=; Max-Age=0`,
+                    `nickname=; Max-Age=0`
+                ],
+                Location: '/'
+            });
+            response.end();
         });
     } else {
         response.writeHead(404);
